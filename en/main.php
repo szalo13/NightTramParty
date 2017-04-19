c45c045e1786e3f136adf98ad01c549ba04e10cd
szalo13 <kamil_sza@yahoo.pl>
2017-04-19 10:56:09 +0200

    Editing english main section content

--- en/main.php
+++ en/main.php
@@ -34,12 +34,12 @@
 <div class="sec-2">
     <div class="container">
         <div class="buy-btn">
-		
+
 		<a href="https://www.eventbrite.com/o/night-tram-party-11730300313">Eventbrite SYSTEM<br>Buy Ticket</a>
 		<br>
-		<br>	
-		<a href="http://buyticket.nighttramparty.com">BUYTICKET SYSTEM<br>Buy Ticket</a>	
-			
+		<br>
+		<a href="http://buyticket.nighttramparty.com">BUYTICKET SYSTEM<br>Buy Ticket</a>
+
         </div>
     </div>
 </div>
@@ -57,7 +57,7 @@
 
 -->
 
-<div class="sec-3">
+<div class="sec-3 container">
     <div class="social">
         <li>
             <a href="https://www.facebook.com/NightTramParty/?fref=ts">
@@ -70,23 +70,23 @@
             </a>
         </li>
     </div>
-	
-	
-	
-    <div class="info-box">
-        <h1>During winter season</h1>
-        <span class="text1">we hold parties only occasionally</span>
-			<span class="text1">- - -</span>
-		<span class="text1">We are in the middle
-of preparing<br>the schedule for <b>April - October 2017</b>.<br>Please feel free to
-contact us<br>via telephone or email<br>regarding parties in this season.</span>
 
-		
-        <span class="text2"><a href="events.php">Check out our schedule</a></span>
+
+
+    <div class="info-box">
+        <span class="text1">We invite you for the best night tram ride of all times which joins partying and night sightseeing.        </span>
+			<span class="text1">- - -</span>
+		<span class="text1">Discover the amazing <b>Cracow's nightlife</b> which is probably the most exciting in Europe. Get on the party tram or boat board with us and give yourself a chance to <b>meet travelers from around the world!</b>    </span>
+    <span class="text1">
+      The two hours' Night Tram Party ride is followed by the <b>pub crawl!</b> Our crew will take you to the best local clubs and pubs where <b>the party is going to be continued.</b>
+    </span>
+
+
+        <span class="text2"><a href="events.php">Buy a ticket now! The number of them is limited.        </span>
     </div>
-	
+
     <iframe src="//embedsocial.com/facebook_album/album_photos/581726492019390" width="90%" height="1500" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
-    
+
     <iframe src="//embedsocial.com/facebook_album/album_photos/572550596270313" width="90%" height="1500" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
 
 	</div>
